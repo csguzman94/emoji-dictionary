37f39cbf2bcbcbc1285fc5ae2c66fe42dd0737f1
Cody Guzman <codyguzman@Codys-MBP-2.attlocal.net>
2017-05-09 21:41:49 -0400

    Emojis and new view control

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,11 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    // tableview outlet
+    @IBOutlet weak var tableview: UITableView!
+    
+    
+    // emoji array
+    var emojis = ["😊","😎","🤑","😡","😈","😷"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // set tableview declarations
+        tableview.dataSource = self
+        tableview.delegate = self
+    }
+    
+    // function for the number of cells
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // returns the size of the emojis array
+        return emojis.count
+    }
+    
+    // function that declares whats in each cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // creates a let constant for each of the cells
+        let cell = UITableViewCell()
+        
+        
+        //insert cell text to each cell
+        //indexPath.row - pulls each element from the list
+        cell.textLabel?.text = emojis[indexPath.row]
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
